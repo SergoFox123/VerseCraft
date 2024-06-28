471f03d6499a0d2037aa0ce5034e9afc989d6889
SergoFox123 <SergoFox123@users.noreply.github.com>
2024-06-28 04:16:03 +0000

    Apply automatic changes

---
+++ build/quilt/licenser/src/main/java/net/sergofox123/vercecraft/BoatTypeVerse.java
@@ -0,0 +1,14 @@
+package net.sergofox123.vercecraft;
+
+import net.minecraft.world.entity.vehicle.Boat;
+
+public final class BoatTypeVerse {
+
+	public static Boat.Type AZALEA;
+
+
+	static {
+		Boat.Type.values();
+	}
+
+}
