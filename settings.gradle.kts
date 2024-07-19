e450acc3f07267f038e442a3c015660ba47e02bc
SergoFox <sergejosadci@gmail.com>
2024-07-20 02:26:42 +0300

    recipes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,28 @@
 pluginManagement {
-	repositories {
-		maven {
-			name = "Quilt"
-			setUrl("https://maven.quiltmc.org/repository/release/")
-		}
-		maven {
-			name = "Quilt Snapshot"
-			setUrl("https://maven.quiltmc.org/repository/snapshot/")
-		}
-		maven {
-			name = "Fabric"
-			setUrl("https://maven.fabricmc.net/")
-		}
-		maven {
-			name = "Forge"
-			setUrl("https://files.minecraftforge.net/maven/")
-		}
-		maven {
-			name = "Jitpack"
-			setUrl("https://jitpack.io/")
-		}
-		mavenCentral()
-		gradlePluginPortal()
-	}
+    repositories {
+        maven {
+            name = "Quilt"
+            setUrl("https://maven.quiltmc.org/repository/release/")
+        }
+        maven {
+            name = "Quilt Snapshot"
+            setUrl("https://maven.quiltmc.org/repository/snapshot/")
+        }
+        maven {
+            name = "Fabric"
+            setUrl("https://maven.fabricmc.net/")
+        }
+        maven {
+            name = "Forge"
+            setUrl("https://files.minecraftforge.net/maven/")
+        }
+        maven {
+            name = "Jitpack"
+            setUrl("https://jitpack.io/")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 
 rootProject.name = "VerceCraft"
