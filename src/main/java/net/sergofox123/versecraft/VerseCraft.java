0f6afd84ad5f1c849b2c87bb4b632a1080714f72
SergoFox <sergejosadci@gmail.com>
2024-08-02 21:21:28 +0300

    Big Changes

---
+++ src/main/java/net/sergofox123/versecraft/VerseCraft.java
@@ -0,0 +1,55 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.sergofox123.versecraft;
+
+import java.util.ArrayList;
+import net.fabricmc.loader.api.ModContainer;
+import net.frozenblock.lib.entrypoint.api.FrozenModInitializer;
+import net.frozenblock.lib.mobcategory.api.entrypoint.FrozenMobCategoryEntrypoint;
+import net.frozenblock.lib.mobcategory.impl.FrozenMobCategory;
+import net.sergofox123.versecraft.registry.RegisterBlocks;
+
+import net.sergofox123.versecraft.registry.RegisterItems;
+import org.jetbrains.annotations.NotNull;
+
+public final class VerseCraft extends FrozenModInitializer implements FrozenMobCategoryEntrypoint {
+
+	public VerseCraft() {
+		super(VerseSharedConstants.MOD_ID);
+	}
+
+	@Override
+	public void onInitialize(String modId, ModContainer container) {
+		VerseSharedConstants.startMeasuring(this);
+
+
+		RegisterBlocks.registerBlocks();
+
+		RegisterItems.registerItems();
+		RegisterItems.registerBlockItems();
+		RegisterItems.init();
+
+		RegisterBlocks.registerBlockProperties();
+
+		VerseSharedConstants.stopMeasuring(this);
+	}
+
+	@Override
+	public void newCategories(@NotNull ArrayList<FrozenMobCategory> context) {
+
+
+	}
+}
---
+++ src/main/java/net/sergofox123/versecraft/mixin/entity/boat/BoatDropsMixin.java
@@ -0,0 +1,37 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.sergofox123.versecraft.mixin.entity.boat;
+
+import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.item.Item;
+import net.sergofox123.versecraft.BoatTypeVerse;
+import net.sergofox123.versecraft.registry.RegisterItems;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Boat.class)
+public class BoatDropsMixin {
+
+	@Inject(method = "getDropItem", at = @At("RETURN"), cancellable = true)
+	public void verseCraft$getModdedBoats(CallbackInfoReturnable<Item> info) {
+		var boat = Boat.class.cast(this);
+		if (boat.getVariant() == BoatTypeVerse.AZALEA) {
+			info.setReturnValue(RegisterItems.AZALEA_BOAT);
+		}
+	}
+}
