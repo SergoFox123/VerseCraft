280e771b28ce292752e2f8d71ad3ffe1dc5d4e59
SergoFox <sergejosadci@gmail.com>
2026-02-26 22:19:31 +0200

    Added checkstyle

--- build.gradle.kts
+++ build.gradle.kts
@@ -495,9 +495,3 @@ val publishMod by tasks.register("publishMod") {
     dependsOn(github)
     dependsOn(tasks.modrinth)
 }
-
-allprojects {
-    tasks.matching { it.name.startsWith("checkstyle") }.configureEach {
-        enabled = false
-    }
-}
